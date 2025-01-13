c49cf421991fba5bc80179b6e9a63d50f64ae989
TaoZeyu <i@taozeyu.com>
2025-01-13 16:33:05 +0800

    feat(blocks): add metadata of PDF

--- tasks/images2pdf/main.py
+++ tasks/images2pdf/main.py
@@ -1,11 +1,14 @@
 import os
 
 from oocana import Context
+from typing import Any
 from tempfile import TemporaryDirectory
 from PyPDF2 import PdfMerger
 from PIL import Image
 
 def main(params: dict, context: Context):
+  title: str | None = params["title"]
+  author: str | None = params["author"]
   image_paths: str = params["image_paths"]
   pdf_file_path: str | None = params["pdf_file_path"]
 
@@ -14,6 +17,12 @@ def main(params: dict, context: Context):
       context.session_dir,
       f"{context.job_id}.pdf",
     )
+  metadata: dict[str, Any] = {}
+  if title is not None:
+    metadata["/Title"] = title
+  if author is not None:
+    metadata["/Author"] = author
+
   with TemporaryDirectory() as temp_dir:
     with PdfMerger() as merger:
       for i, image_path in enumerate(image_paths):
@@ -26,6 +35,9 @@ def main(params: dict, context: Context):
         )
         merger.append(page_path)
         context.report_progress(i/len(image_paths) * 90.0)
+
+      if len(metadata) > 0:
+        merger.add_metadata(metadata)
       merger.write(pdf_file_path)
     
   return { "pdf_file_path": pdf_file_path }
