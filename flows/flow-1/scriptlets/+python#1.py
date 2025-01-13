a356c991e5c0c78307a36a6bcd55920b4e9f2032
TaoZeyu <i@taozeyu.com>
2025-01-13 15:34:51 +0800

    feat(block): migrate to reportlab

--- flows/flow-1/scriptlets/+python#1.py
+++ flows/flow-1/scriptlets/+python#1.py
@@ -1,20 +1,4 @@
-from oocana import Context
-
-# "in", "out" is the default node key.
-# Redefine the name and type of the node, change it manually below.
-# Click on the gear(⚙) to configure the input output UI
-
-def main(inputs: dict, context: Context):
-  # inputs.get("in") -> help you get node input value
-
-  # preview pandas dataframe
-  # context.preview(df)
-
-  # context.preview({
-  #   # type can be "image", "video", "audio", "markdown", "table", "iframe"
-  #   "type": "image",
-  #   # data can be file path, base64, pandas dataframe
-  #   "data": "",
-  # })
-
-  return { "out": None }
+def main(params: dict):
+  image_paths: list[str] = params["image_paths"]
+  image_paths.sort()
+  return { "image_paths": image_paths }
---
+++ tasks/images2pdf/main.py
@@ -0,0 +1,30 @@
+import os
+
+from oocana import Context
+from tempfile import TemporaryDirectory
+from PyPDF2 import PdfMerger
+from PIL import Image
+
+def main(params: dict, context: Context):
+  image_paths: str = params["image_paths"]
+  pdf_file_path: str | None = params["pdf_file_path"]
+
+  if pdf_file_path is None:
+    pdf_file_path = os.path.join(
+      context.session_dir,
+      f"{context.job_id}.pdf",
+    )
+  with TemporaryDirectory() as temp_dir:
+    with PdfMerger() as merger:
+      for i, image_path in enumerate(image_paths):
+        image = Image.open(image_path).convert()
+        page_path = os.path.join(temp_dir, f"{i}.pdf")
+        page_width, page_height = image.size
+        image.save(
+          fp=page_path, 
+          save_all=True,
+        )
+        merger.append(page_path)
+      merger.write(pdf_file_path)
+    
+  return { "pdf_file_path": pdf_file_path }
