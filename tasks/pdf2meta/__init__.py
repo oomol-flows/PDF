61aa653387cc629bdf5caaafd392b3ee121d6fb6
TaoZeyu <i@taozeyu.com>
2025-03-25 11:20:43 +0800

    feat(flows): add pdf2meta flow & interfaces

---
+++ tasks/pdf2meta/__init__.py
@@ -0,0 +1,15 @@
+#region generated meta
+import typing
+class Inputs(typing.TypedDict):
+  pdf: str
+class Outputs(typing.TypedDict):
+  meta: dict
+#endregion
+
+from oocana import Context
+
+def main(params: Inputs, context: Context) -> Outputs:
+
+  # your code
+
+  return { "output": "output_value" }
