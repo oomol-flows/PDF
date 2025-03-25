2a30eb88ac3f739c07877ae08f463bacaf2dc03a
TaoZeyu <i@taozeyu.com>
2025-03-25 11:33:11 +0800

    feat(tasks): implements pdf2meta

--- tasks/pdf2meta/__init__.py
+++ tasks/pdf2meta/__init__.py
@@ -1,15 +1,26 @@
-#region generated meta
+import os
 import typing
+import PyPDF2
+
+
 class Inputs(typing.TypedDict):
   pdf: str
+
 class Outputs(typing.TypedDict):
+  name: str
   meta: dict
-#endregion
 
-from oocana import Context
+def main(params: Inputs) -> Outputs:
+  pdf_path = params["pdf"]
+  pdf_reader = PyPDF2.PdfReader(pdf_path)
+  metadata = pdf_reader.metadata
+  metadata_dict: dict[str, str] = {}
+  if metadata is not None:
+    for key, value in metadata.items():
+      clean_key = key.lstrip("/") if key.startswith("/") else key
+      metadata_dict[clean_key] = f"{value}"
 
-def main(params: Inputs, context: Context) -> Outputs:
-
-  # your code
-
-  return { "output": "output_value" }
+  return {
+    "name": os.path.splitext(os.path.basename(pdf_path))[0],
+    "meta": metadata_dict,
+  }
