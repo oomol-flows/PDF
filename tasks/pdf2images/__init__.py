6bab373f257ce46baf6a1d64c107dca3cbaba4b4
shaun <beingswu@gmail.com>
2025-06-11 10:43:11 +0000

    update pdf2image

--- tasks/pdf2images/__init__.py
+++ tasks/pdf2images/__init__.py
@@ -1,31 +1,37 @@
+import os
 from oocana import Context
 from pdf2image import convert_from_path
 
 
 def main(params: dict, context: Context):
-  pdf_path = params.get("pdf_path")
-  image_dir = params.get("image_dir")
+    pdf_path = params.get("pdf_path")
+    image_dir = params.get("image_dir")
 
-  if pdf_path is None:
-    raise ValueError("pdf_path is required")
+    if pdf_path is None:
+        raise ValueError("pdf_path is required")
 
-  if image_dir is None:
-    image_dir = context.session_dir
+    if image_dir is None:
+        image_dir = os.path.join(
+            context.session_dir,
+            context.job_id,
+        )
+    os.makedirs(image_dir, exist_ok=True)
 
-  pdf_to_images(pdf_path, image_dir, context)
+    pdf_to_images(pdf_path, image_dir, context)
 
-  return { "image_dir": image_dir }
+    return {"image_dir": image_dir}
+
 
 def pdf_to_images(pdf_path, output_folder, context):
-  # Convert PDF to images
-  images = convert_from_path(pdf_path)
-  total_pages = len(images)
-  percentage = 0
-  # Save each image
-  for i, image in enumerate(images):
-    # Calculate the percentage of the current page
-    percentage = (i + 1) / total_pages * 100
-    # Print or record percentage
-    context.report_progress(percentage)
-    image_path = f"{output_folder}/page_{i + 1}.png"
-    image.save(image_path, "PNG")
+    # Convert PDF to images
+    images = convert_from_path(pdf_path)
+    total_pages = len(images)
+    percentage = 0
+    # Save each image
+    for i, image in enumerate(images):
+        # Calculate the percentage of the current page
+        percentage = (i + 1) / total_pages * 100
+        # Print or record percentage
+        context.report_progress(percentage)
+        image_path = f"{output_folder}/page_{i + 1}.png"
+        image.save(image_path, "PNG")
